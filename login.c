450aa2fd2e02432fe43c70f897ce1d1ff4d1c6e9
cj-muse <Connermattingly@yahoo.com>
2016-04-16 12:09:38 -0700

    Built and ready to change

---
+++ login.c
@@ -0,0 +1,35 @@
+char *tty;
+
+main(int argc, char *argv[])   // invoked by exec("login /dev/ttyxx")
+{
+	tty =  argv[1];
+
+	printf("CM_LOGIN: ");
+	//close(0); close(1); close(2); // login process may run on different terms
+
+	// open its own tty as stdin, stdout, stderr
+
+	settty(tty);   // store tty string in PROC.tty[] for putc()
+
+	//NOW we can use printf, which calls putc() to our tty
+   printf("CM_LOGIN : open %s as stdin, stdout, stderr\n", tty);
+
+	signal(2,1);  // ignore Control-C interrupts so that 
+                // Control-C KILLs other procs on this tty but not the main sh
+
+	while(1){
+		//1. show login:           to stdout
+   	//2. read user nmae        from stdin
+	   //3. show passwd:
+ 		//4. read user passwd
+
+    	//5. verify user name and passwd from /etc/passwd file
+
+    	//6. if (user account valid){
+      	//setuid to user uid.
+      	//chdir to user HOME directory.
+      	//exec to the program in users's account
+      	//}
+       	//printf("login failed, try again\n");
+	}
+}
---
+++ q
@@ -0,0 +1,7 @@
+qemu-system-x86_64 -hda vdisk -m 520m -smp 2 -serial /dev/pts/12 -serial /dev/pts/13 \
+-parallel /dev/pts/3 -localtime 
+
+#not using the mtx install iso 
+#-cdrom MTXinstallCD.iso #-boot d
+
+
